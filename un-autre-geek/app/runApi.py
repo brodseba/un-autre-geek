7d3fec41db431f1832c9fb202cab5f298069efce
brodseba <brodseba@penguin>
2020-08-31 23:38:36 -0400

    Fixing templates location

--- un-autre-geek/app/runApi.py
+++ un-autre-geek/app/runApi.py
@@ -3,7 +3,7 @@ from v1.app import api_bp as app_v1
 import os
 
 app = flask.Flask(__name__)
-app.register_blueprint(app_v1, url_prefix='/app/v1', template_folder="/app/v1/templates")
+app.register_blueprint(app_v1, url_prefix='/app/v1', template_folder="templates")
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
