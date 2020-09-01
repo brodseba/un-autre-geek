7eac6b95cf02eec95320b9bcefb2cce355e7269c
brodseba <brodseba@penguin>
2020-08-31 23:08:24 -0400

    Third fix

--- un-autre-geek/app/v1/app.py
+++ un-autre-geek/app/v1/app.py
@@ -59,17 +59,17 @@ class Places(Resource):
 api.add_resource(Places, '/places')
 
 
-@app.errorhandler(404)
+@api_bp.errorhandler(404)
 def resource_not_found(e):
     return jsonify(error=str(e)), 404
 
 
-@app.route('/')
-@app.route('/<name>')
+@api_bp.route('/')
+@api_bp.route('/<name>')
 def hello(name=None):
     return render_template('hello.html', name=name)
 
 
-@app.route('/list')
+@api_bp.route('/list')
 def list(data=None):
     return render_template('places.html', data=DATA)
