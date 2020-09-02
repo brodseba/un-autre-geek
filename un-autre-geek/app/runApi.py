029aaa6bafffbaaa8013beb18a580905c6c375da
brodseba <brodseba@penguin>
2020-09-02 11:53:51 -0400

    Better html

--- un-autre-geek/app/runApi.py
+++ un-autre-geek/app/runApi.py
@@ -3,8 +3,19 @@ from v1.app import api_bp as app_v1
 from v2.app import api_bp as app_v2
 import os
 import json
+from google.cloud import firestore
 
 app = Flask(__name__)
+
+# Project ID is determined by the GCLOUD_PROJECT environment variable
+db = firestore.Client()
+
+doc_ref = db.collection(u'v1')
+
+docs = doc_ref.stream()
+DATA = {'places' : []}
+for doc in docs:
+    DATA["places"].append(doc.to_dict()["name"])
 
 @app.errorhandler(404)
 def resource_not_found(e):
@@ -14,7 +25,7 @@ def resource_not_found(e):
 @app.route('/')
 @app.route('/<name>')
 def hello(name=None):
-    return render_template('hello.html', name=name)
+    return render_template('hello.html', name=name, data=DATA)
 
 app.register_blueprint(app_v1, url_prefix='/app/v1')
 app.register_blueprint(app_v2, url_prefix='/app/v2')
