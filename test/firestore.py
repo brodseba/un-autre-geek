e9d782308cfd3fd957b36f00d158978d5a6e01a8
brodseba <brodseba@penguin>
2020-09-02 00:42:53 -0400

    test

--- test/firestore.py
+++ test/firestore.py
@@ -5,11 +5,12 @@ from google.cloud import firestore
 # Project ID is determined by the GCLOUD_PROJECT environment variable
 db = firestore.Client()
 
-doc_ref = db.collection(u'v1')
+docs = db.collection(u'v1').stream()
 
-docs = doc_ref.stream()
 DATA = {'places' : []}
 for doc in docs:
     DATA["places"].append(doc.to_dict()["name"])
 
 print(DATA)
+
+db.collection(u'v1').document().set({"name": "test"})
